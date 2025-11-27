806d42e0ac613bbdb4c9d0046a1fac680d227dd2
drbo6 <bobdeschutter6@gmail.com>
2025-11-27 14:25:17 -0500

    Precons seem to load correctly

---
+++ precons/Fight or Flight.js
@@ -0,0 +1,28 @@
+// Exported preconstructed deck
+registerPrecon({
+    name: "Fight or Flight",
+    identity: "31002",
+    cards: {
+        "30002": 3,  // Wildcat Strike
+        "30004": 3,  // Botulus
+        "30005": 1,  // Buzzsaw
+        "30007": 3,  // Fermenter
+        "30009": 3,  // Cookbook
+        "30012": 3,  // Tread Lightly
+        "30013": 1,  // Docklands Pass
+        "30014": 1,  // Pennyshaver
+        "30017": 2,  // Tranquilizer
+        "30030": 3,  // Sure Gamble
+        "30031": 2,  // T400 Memory Diamond
+        "30032": 1,  // Mayfly
+        "30034": 1,  // Verbal Plasticity
+        "31003": 3,  // En Passant
+        "31004": 2,  // Retrieval Run
+        "31006": 1,  // Corroder
+        "31010": 3,  // Liberated Account
+        "31012": 2,  // Xanadu
+        "31015": 1,  // Career Fair
+        "31037": 3,  // Dirty Laundry
+        "31039": 3// Earthrise Hotel
+    }
+});
